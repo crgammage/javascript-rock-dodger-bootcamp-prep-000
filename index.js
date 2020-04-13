b9234a6fb42606d4f2a9438a1300a3bd3c91776e
masterful-array-3850 <crgammage@gmail.com>
2020-04-13 16:04:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -65,9 +65,6 @@ function createRock(x) {
   var top = 0
 
   rock.style.top = top
-window.requestAnimationFrame() {
-  rock.append('GAME')
-}
   /**
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
